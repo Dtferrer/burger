b6f0319dc8a3a5995dedb662c96d30967018aeb5
Danica Ferrer <dtferrer@ucsd.edu>
2019-09-28 19:19:37 -0700

    Basic website and ability to devour burger up.

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured) 
+VALUES("Blue Cheese Burger", False);
+
+INSERT INTO burgers(burger_name, devoured) 
+VALUES("Bacon Burger", False);
+
+INSERT INTO burgers(burger_name, devoured) 
+VALUES("Vegan Burger", False);
